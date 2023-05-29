7fa549425eccd85cfb48a98f724793147b122aaa
Your Name <raphael.hendrigo@gmail.com>
2023-05-29 13:14:13 -0300

    segundo commit

--- app.py
+++ app.py
@@ -1,7 +1,17 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
+from flask import Flask
+
+
+import os
+
+if os.environ.get('DATABASE_URL'):
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace("://", "ql://", 1)
+else:
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///your-local-sqlite-database.db'
 
 app = Flask(__name__)
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///C:/Users/rapha/boletim/students.db'
 db = SQLAlchemy(app)
 
